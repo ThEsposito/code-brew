7de1109cd49a8ad4a0e0af2ba01980972020f56b
ThEsposito <thesposito.estudos@gmail.com>
2025-07-04 18:44:12 -0300

    Modificador final pt2

--- src/academy/devdojo/maratonajava/javacore/J_modificadorFinal/domain/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/J_modificadorFinal/domain/Carro.java
@@ -3,14 +3,13 @@ package academy.devdojo.maratonajava.javacore.J_modificadorFinal.domain;
 public class Carro {
     private String nome;
     public static final double VELOCIDADE_LIMITE;
-    private final Comprador COMPRADOR = new Comprador();
+    public final Comprador COMPRADOR = new Comprador();
     // Se n colcoar o new Comprador() vai dar uma exceção.
     // Já q obriga a inicializar a variável.
 
     static {
         VELOCIDADE_LIMITE = 250;
     }
-
     public String getNome() {
         return nome;
     }
--- src/academy/devdojo/maratonajava/javacore/J_modificadorFinal/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/J_modificadorFinal/test/CarroTest01.java
@@ -1,10 +1,24 @@
 package academy.devdojo.maratonajava.javacore.J_modificadorFinal.test;
 
 import academy.devdojo.maratonajava.javacore.J_modificadorFinal.domain.Carro;
+import academy.devdojo.maratonajava.javacore.J_modificadorFinal.domain.Comprador;
 
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
         System.out.println(Carro.VELOCIDADE_LIMITE);
+//      Carro.VELOCIDADE_LIMITE = 2; Daria pau, já que o valor da var já foi atribuído
+
+        System.out.println(carro.COMPRADOR);
+
+/*      Comprador comp1 = new Comprador();
+        carro.COMPRADOR = comp1;
+
+        Não deixa, já que carro.COMPRADOR já foi inicializado (com o construtor)
+        dentro do escopo da classe
+ */
+        carro.COMPRADOR.setNome("Theozão comprador de fusca");
+        System.out.println(carro.COMPRADOR);
+        // Os atributos de objetos final podem ser reatribuídos livremente
     }
 }
