b9782538ed490875869561a87d5dd88e9eb1f1ac
ThEsposito <thesposito.estudos@gmail.com>
2025-07-05 00:04:49 -0300

    Enums pt3 - Sobrescrita de métodos

--- src/academy/devdojo/maratonajava/javacore/K_enum/test/ClienteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/K_enum/test/ClienteTest01.java
@@ -12,5 +12,6 @@ public class ClienteTest01 {
         System.out.println(cliente1);
         System.out.println(cliente2);
         System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
     }
 }
