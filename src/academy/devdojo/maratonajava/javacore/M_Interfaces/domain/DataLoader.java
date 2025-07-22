523250b15191e07995391361b1e6c3590b90246a
ThEsposito <thesposito.estudos@gmail.com>
2025-07-22 16:02:09 -0300

    Interfaces pt1

--- src/academy/devdojo/maratonajava/javacore/M_Interfaces/domain/DataLoader.java
+++ src/academy/devdojo/maratonajava/javacore/M_Interfaces/domain/DataLoader.java
@@ -2,8 +2,7 @@ package academy.devdojo.maratonajava.javacore.M_Interfaces.domain;
 
 public interface DataLoader {
     void load();
-    // Já é um métdo abstrato, mesmo sem ter especificado.
-    // Por padrão são todos públicos e abstratos
+    // Por padrão todos os métodos de uma interface são todos públicos e abstratos
 
     // Interfaces foram criadas com o intuito de prover um contrato para implementações dela.
 }
