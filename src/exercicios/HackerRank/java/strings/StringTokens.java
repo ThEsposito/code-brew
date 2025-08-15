45c3e5563df7d1fa4d53e81c7bc911c50cd78f59
ThEsposito <thesposito.estudos@gmail.com>
2025-08-15 19:59:43 -0300

    Refatorando pacote de exercícios

--- src/exercicios/HackerRank/java/strings/StringTokens.java
+++ src/exercicios/HackerRank/java/strings/StringTokens.java
@@ -6,12 +6,17 @@ public class StringTokens {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
-        String[] arr = s.split("[ !,?._'@]+");
 
-        System.out.println(arr.length);
-        for(String word : arr) System.out.println(word);
+        String trimmed = s.trim();
 
+        if(trimmed.isEmpty()){
+            System.out.println(0);
+        } else {
+            String[] arr = trimmed.split("[ !,?._'@]+");
+            System.out.println(arr.length);
+
+            for(String word : arr) System.out.println(word);
+        }
         scan.close();
     }
-
 }
