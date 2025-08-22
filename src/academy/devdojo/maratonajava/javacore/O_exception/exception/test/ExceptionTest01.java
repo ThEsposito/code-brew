256eefac6d7f5bd7e70297bc3665a166be8c2d46
ThEsposito <thesposito.estudos@gmail.com>
2025-08-22 09:09:08 -0300

    Exceções pt 3

--- src/academy/devdojo/maratonajava/javacore/O_exception/exception/test/ExceptionTest01.java
+++ src/academy/devdojo/maratonajava/javacore/O_exception/exception/test/ExceptionTest01.java
@@ -1,5 +1,26 @@
 package academy.devdojo.maratonajava.javacore.O_exception.exception.test;
 
-public class ExceptionTest01 {
+import java.io.File;
+import java.io.IOException;
 
+public class ExceptionTest01 {
+    public static void main(String[] args) {
+        criarNovoArquivo();
+    }
+
+    private static void criarNovoArquivo(){
+        File file = new File("arquivo/teste.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado: "+isCriado);
+        } catch (IOException e) {
+            // A stackTrace mostra exatamente onde o erro/exceção ocorreu, mas não interrompe
+            // o programa.
+            e.printStackTrace();
+            // NUNCA DEIXE O BLOCO CATCH EM BRANCO!!!
+            // Evite colocar regra de negócio no catch!
+        }
+        System.out.println("Depois do bloco try");
+
+    }
 }
