305ee7d4b685a97ca0e61b868a377fe45475bb4b
ThEsposito <thesposito.estudos@gmail.com>
2025-08-24 10:29:16 -0300

    Geeks4Geeks projetc I: Number Guessing Game

--- src/exercicios/geekforgeeks/projects/basics/NumberGuessingGame.java
+++ src/exercicios/geekforgeeks/projects/basics/NumberGuessingGame.java
@@ -1,5 +1,24 @@
 package exercicios.geekforgeeks.projects.basics;
+/*
+RULES OF THE GAME:
+    - If the guessed number is bigger than the actual number, the program will respond with the message that the
+      guessed number is higher than the actual number.
+    - If the guessed number is smaller than the actual number, the program will respond with the message that the
+      guessed number is lower than the actual number.
+    - If the guessed number is equal to the actual number or if the K trials are exhausted, the program will end
+      with a suitable message.
 
+IMPLEMENTATION DETAILS:
+    - The program generates a random number between a predefined range (e.g., 1 to 100).
+    - The user has limited attempts (K tries) to guess the number.
+    - At each guess, the program provides a hint:
+        - If the guessed number is higher, it tells the user to guess lower.
+        - If the guessed number is lower, it tells the user to guess higher.
+    - If the user guesses correctly, they win.
+    - If all attempts are exhausted, the game reveals the correct number.
+
+More details: https://www.geeksforgeeks.org/dsa/number-guessing-game-in-java/
+ */
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
