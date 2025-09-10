63af88b266c6f22f9f1851968776060db7bfe71a
ThEsposito <thesposito.estudos@gmail.com>
2025-09-10 11:46:29 -0300

    Curso Nélio Alves - Relembrando enums e método valueOf()

---
+++ src/nelio/alves/s12_enums_composicao/enumeracoes/Main.java
@@ -0,0 +1,12 @@
+package nelio.alves.s12_enums_composicao.enumeracoes;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Order order = new Order(1010, new Date(), OrderStatus.PENDING_PAYMENT);
+        System.out.println(order);
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+        System.out.println(os2);
+    }
+}
---
+++ src/nelio/alves/s12_enums_composicao/enumeracoes/OrderStatus.java
@@ -0,0 +1,8 @@
+package nelio.alves.s12_enums_composicao.enumeracoes;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
